8f7c6498984a0d722474205491139d3794b857c2
BhavanaSomala01 <125897406+BhavanaSomala01@users.noreply.github.com>
2024-07-08 16:44:08 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Login from "./components/auth/Login";
+import Register from "./components/auth/Register";
+import { ToastContainer } from "react-toastify";
+import Home from "./components/dash/Home";
+import Navbar from "./components/Navbar";
+import PageNotFound from "./components/reusable/PageNotFound";
+import Talk from "./components/dash/Talk";
+import ScheduleAppointment from "./components/dash/ScheduleAppointment";
+import BMICalculator from "./components/dash/BMICalculator";
+import DietAndTips from "./components/dash/DietAndTips";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/home" element={<Home />}>
+          <Route path="talk" element={<Talk />} />
+          <Route path="diet-and-tips" element={<DietAndTips />} />
+          <Route
+            path="schedule-appointment"
+            element={<ScheduleAppointment />}
+          />
+          <Route path="bmi" element={<BMICalculator />} />
+        </Route>
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+      <ToastContainer />
+    </BrowserRouter>
   );
 }
 
